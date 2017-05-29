626844bc1956c0ef484603d8f41a4af1c91bb583
stigie <stiggie@info.sgu.ru>
2017-05-29 16:56:20 +0400

    --reedit-message=first_working_version

--- dlink/main.go
+++ dlink/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"html/template"
-	textTemplate "text/template"
+	"bufio"
 	"encoding/json"
 	"flag"
+	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	sw "strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -45,7 +48,7 @@ type User struct {
 	UserLog   int
 	UserPas   string
 	UserName  string
-	AnableReg bool
+	EnableReg bool
 }
 
 type PhoneConf struct {
@@ -54,29 +57,107 @@ type PhoneConf struct {
 	VlanPhoneNumb int
 	VlanComp      bool
 	VlanCompNumb  int
-	Version       string
+	Version       float64
 }
 
 type server struct {
 	Db *sqlx.DB
 }
 
-func (c *PhoneConf) MakeConfig(pf *Phone) (string, error) {
-	latexTemplate, err := textTemplate.ParseFiles("TelConfig.xml")
-	if err != nil {
-		return "", err
+//function from makeConfig pakage
+func newline() string {
+	return "\n            "
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
 	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func (c *PhoneConf) MakeConfig(pf *Phone) (string, error) {
+	fmap := template.FuncMap{
+		"addOne":  addOne,
+		"newline": newline,
+	}
+	latexTemplate := template.Must(template.New("Config template").Funcs(fmap).ParseFiles("TelConfig.xml"))
+	//latexTemplate, err := template
+	/*if err != nil {
+		return "", err
+	}*/
+
+	if path, err := exists("userFiles/" + pf.Mac + ".xml"); err == nil && path == true {
+		file, _ := os.Open("userFiles/" + pf.Mac + ".xml")
+		f := bufio.NewReader(file)
+		var i = 0;
+		var read_line string;
+		for i < 3 {
+			read_line, _ = f.ReadString('\n')
+			i++;
+		}
+
+		file.Close()
+		start := sw.Index(read_line, ">");
+		read_line = read_line[start+1:len(read_line)];
+		start = sw.Index(read_line, "<");
+		read_line = cut(read_line, start);
+		version, err := strconv.ParseFloat(read_line, 64)
+		if err != nil {
+			log.Println(err)
+		}
+		version+=0.0001
+		//fmt.Println(version);
+		c.Version = version;
+
+	}
+
 	outputLatexFile, err := os.Create("userFiles/" + pf.Mac + ".xml")
 	if err != nil {
 		return "", err
 	}
+	//log.Print(err);
 	defer outputLatexFile.Close()
-	err = latexTemplate.ExecuteTemplate(outputLatexFile, "TelConfig.xml", c)
+	err = latexTemplate.ExecuteTemplate(outputLatexFile, "TelConfig.xml", createStatement(c))
 	if err != nil {
 		return "", err
 	}
 	pathToTexFile := "userFiles\\" + pf.Mac + ".xml"
 	return pathToTexFile, nil
+}
+
+func addOne(number int) int {
+	return number + 1
+}
+
+func createStatement(c *PhoneConf) PhoneConf {
+	return PhoneConf{
+		VlanPhone:     c.VlanPhone,
+		VlanPhoneNumb: c.VlanPhoneNumb,
+		VlanComp:      c.VlanComp,
+		VlanCompNumb:  c.VlanCompNumb,
+		Version:       c.Version,
+		Users: [2]User{
+			User{
+				UserLog:   c.Users[0].UserLog,
+				UserPas:   c.Users[0].UserPas,
+				UserName:  c.Users[0].UserName,
+				EnableReg: c.Users[0].EnableReg,
+
+			},
+			User{
+				UserLog:   c.Users[1].UserLog,
+				UserPas:   c.Users[1].UserPas,
+				UserName:  c.Users[1].UserName,
+				EnableReg: c.Users[1].EnableReg,
+
+			},
+		},
+	}
 }
 
 func loadConfig(path string) error {
@@ -115,13 +196,87 @@ func (s *server) indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 func (s *server) execHandler(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
-	//fmt.Fprint(w, r.PostForm)
+	fmt.Fprint(w, r.PostForm)
 	if err != nil {
 		log.Print(err)
 		return
 	}
-}
+	_ = r.PostFormValue("ff")
+	forma := r.PostForm;
+	var SIP1, SIP2, vlanIDtel, vlanIDcomp int;
+	if SIP1, err = strconv.Atoi(forma.Get("userPhoneNumber1")); err != nil || (SIP1 < 1000 && SIP1 > 9999) {
+		log.Println(err)
+		return
+	}
+	if SIP2, err = strconv.Atoi(forma.Get("userPhoneNumber2")); err != nil || (SIP2 < 1000 && SIP2 > 9999) {
+		log.Println(err)
+		return
+	}
 
+	if vlanIDtel, err = strconv.Atoi(forma.Get("vlanIDtel")); err != nil || (vlanIDtel < 1 && vlanIDtel > 4095) {
+		log.Println(err)
+		return
+	}
+	if vlanIDcomp, err = strconv.Atoi(forma.Get("vlanIDcomp")); err != nil || (vlanIDcomp < 1 && vlanIDcomp > 4095) {
+		log.Println(err)
+		return
+	}
+	log.Print(SIP1);
+	log.Print(SIP2);
+	log.Print(vlanIDcomp);
+	log.Println(vlanIDtel);
+	MacAddr := forma.Get("phone");
+	start := sw.Index(MacAddr, " ");
+	MacAddr = MacAddr[start+1:len(MacAddr)];
+	start = sw.Index(MacAddr, " ");
+	MacAddr = cut(MacAddr, start);
+	MacAddr = sw.ToUpper(MacAddr);
+	log.Println(MacAddr)
+	rows, err := s.Db.NamedQuery("SELECT `internalnumber`, `description`, `password` FROM `phones_phone` WHERE `internalnumber` = :sip1 OR  `internalnumber` = :sip2", map[string]interface{}{"sip1": SIP1, "sip2": SIP2})
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	phoneData := SipUser{}
+	SipUserData := []SipUser{}
+	for rows.Next() {
+		err := rows.StructScan(&phoneData)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		SipUserData = append(SipUserData, phoneData)
+
+	}
+	log.Println(SipUserData)
+	//log.Println(phoneData[2])
+
+	u1 := User{SIP1, SipUserData[0].Password, SipUserData[0].Description, true}
+	u2 := User{}
+	if (forma.Get("SecondSipEnable") == "on"){
+		u2 = User{SIP2, SipUserData[1].Password, SipUserData[1].Description, true}
+	}else {
+		u2 = User{0, "", "", false}
+	}
+	x := [2]User{u1, u2}
+	ph := Phone{forma.Get("UserPhone"), MacAddr, "122.123.123.132"}
+	pC := PhoneConf{x, true, 52, true, 9, 2.0006}
+	_, err = pC.MakeConfig(&ph)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	//log.Println(phoneData.);
+	//log.Println(SipUserData[1].Password);
+}
+func cut(text string, limit int) string {
+	runes := []rune(text)
+	if len(runes) >= limit {
+		return string(runes[:limit])
+	}
+	return text
+}
 func main() {
 	flag.Parse()
 	err := loadConfig(*configFile)
@@ -133,15 +288,6 @@ func main() {
 		Db: sqlx.MustConnect("mysql", config.MysqlLogin+":"+config.MysqlPassword+"@tcp("+config.MysqlHost+")/"+config.MysqlDb+"?charset=utf8"),
 	}
 
-	u1 := User{4999, "Ebay", "YAH5", true}
-	u2 := User{0, "", "", false}
-	x := [2]User{u1, u2}
-	ph := Phone{"dlink", "000012121212", "122.123.123.132"}
-	pC := PhoneConf{x, true, 52, true, 9, "2.0006"}
-	_, err = pC.MakeConfig(&ph)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	http.HandleFunc("/", s.indexHandler)
 	http.HandleFunc("/exec/", s.execHandler)
